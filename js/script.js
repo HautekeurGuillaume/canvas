4d80178c5ab2a6f39fe4df2f5f76a4a4cb46a899
HautekeurGuillaume <g.sockersot@gmail.com>
2015-12-15 11:46:25 +0100

    2nd update

--- js/script.js
+++ js/script.js
@@ -4,18 +4,20 @@ var ctx = c.getContext("2d");
 //Cercle//
 ctx.beginPath();
 ctx.arc(200,200,100,0,Math.PI*2);
-ctx.lineWidth = 9;
+ctx.lineWidth = 19;
 ctx.strokeStyle= '#00A5CD';
 ctx.fillStyle= 'white';
 ctx.fill();
 ctx.stroke();
 ctx.lineWidth = 9;
+
+
 //1er carré
 
 ctx.fillStyle = "rgb(12, 127, 221)";
-ctx.fillRect(187,170,25,25);
+ctx.fillRect(187,160,25,25);
 
 //2nd carré
 
-ctx.fillStyle = "rgb(12, 63, 149)";
+ctx.fillStyle = "rgb(53, 99, 186)";
 ctx.fillRect(187,210,25,25);
